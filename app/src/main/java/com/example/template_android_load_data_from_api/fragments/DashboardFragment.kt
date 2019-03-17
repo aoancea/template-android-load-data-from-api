d03efaa89e12657f704ca959f947e030e9c2cfc6
Alexandru Oancea <alx.oancea@gmail.com>
2019-03-17 15:07:07 +0200

    move animal list into its own fragment

--- app/src/main/java/com/example/template_android_load_data_from_api/fragments/DashboardFragment.kt
+++ app/src/main/java/com/example/template_android_load_data_from_api/fragments/DashboardFragment.kt
@@ -2,15 +2,73 @@ package com.example.template_android_load_data_from_api.fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.example.template_android_load_data_from_api.R
+import com.example.template_android_load_data_from_api.adapters.AnimalAdapter
+
+import kotlinx.android.synthetic.main.fragment_dashboard.*
 
 class DashboardFragment : Fragment() {
+
+    private val animals: ArrayList<String> = ArrayList()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Loads animals into the ArrayList
+        addAnimals()
+
+        // Creates a vertical Layout Manager
+        rv_animal_list.layoutManager = LinearLayoutManager(this.requireContext())
+
+        // You can use GridLayoutManager if you want multiple columns. Enter the number of columns as a parameter.
+//        rv_animal_list.layoutManager = GridLayoutManager(this, 2)
+
+        // Access the RecyclerView Adapter and load the data into it
+        rv_animal_list.adapter = AnimalAdapter(animals, this.requireContext())
+    }
+
+    private fun addAnimals() {
+        animals.add("dog")
+        animals.add("cat")
+        animals.add("owl")
+        animals.add("cheetah")
+        animals.add("raccoon")
+        animals.add("bird")
+        animals.add("snake")
+        animals.add("lizard")
+        animals.add("hamster")
+        animals.add("bear")
+        animals.add("lion")
+        animals.add("tiger")
+        animals.add("horse")
+        animals.add("frog")
+        animals.add("fish")
+        animals.add("shark")
+        animals.add("turtle")
+        animals.add("elephant")
+        animals.add("cow")
+        animals.add("beaver")
+        animals.add("bison")
+        animals.add("porcupine")
+        animals.add("rat")
+        animals.add("mouse")
+        animals.add("goose")
+        animals.add("deer")
+        animals.add("fox")
+        animals.add("moose")
+        animals.add("buffalo")
+        animals.add("monkey")
+        animals.add("penguin")
+        animals.add("parrot")
     }
 }

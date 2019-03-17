12a9684327756d2badf3fdb9d9eaa7fe78c2608b
Alexandru Oancea <alx.oancea@gmail.com>
2019-03-17 11:58:04 +0200

    load animals in a recyclerview

--- app/src/main/java/com/example/template_android_load_data_from_api/MainActivity.kt
+++ app/src/main/java/com/example/template_android_load_data_from_api/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.template_android_load_data_from_api
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import com.example.template_android_load_data_from_api.adapters.AnimalAdapter
 import com.example.template_android_load_data_from_api.api.ApiCall
 import com.example.template_android_load_data_from_api.api.IApiCall
 import com.example.template_android_load_data_from_api.models.Issue
@@ -11,6 +13,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private val apiCall: IApiCall = ApiCall()
+
+    val animals: ArrayList<String> = ArrayList()
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
@@ -33,6 +37,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Loads animals into the ArrayList
+        addAnimals()
+
+        // Creates a vertical Layout Manager
+        rv_animal_list.layoutManager = LinearLayoutManager(this)
+
+        // You can use GridLayoutManager if you want multiple columns. Enter the number of columns as a parameter.
+//        rv_animal_list.layoutManager = GridLayoutManager(this, 2)
+
+        // Access the RecyclerView Adapter and load the data into it
+        rv_animal_list.adapter = AnimalAdapter(animals, this)
 
         apiCall.listIssues(object : ApiCall.CallbackInterface<List<Issue>> {
             override fun success(issues: List<Issue>) {
@@ -59,6 +75,41 @@ class MainActivity : AppCompatActivity() {
         })
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    fun addAnimals() {
+        animals.add("dog")
+        animals.add("cat")
+        animals.add("owl")
+        animals.add("cheetah")
+        animals.add("raccoon")
+        animals.add("bird")
+        animals.add("snake")
+        animals.add("lizard")
+        animals.add("hamster")
+        animals.add("bear")
+        animals.add("lion")
+        animals.add("tiger")
+        animals.add("horse")
+        animals.add("frog")
+        animals.add("fish")
+        animals.add("shark")
+        animals.add("turtle")
+        animals.add("elephant")
+        animals.add("cow")
+        animals.add("beaver")
+        animals.add("bison")
+        animals.add("porcupine")
+        animals.add("rat")
+        animals.add("mouse")
+        animals.add("goose")
+        animals.add("deer")
+        animals.add("fox")
+        animals.add("moose")
+        animals.add("buffalo")
+        animals.add("monkey")
+        animals.add("penguin")
+        animals.add("parrot")
     }
 
     companion object {
